edf573f5e5f0faf1cb40fabc23390809b155361d
shizhenyu <imjtrszy@163.com>
2017-11-15 19:35:40 +0800

    修复RSHash函数计算时间过长的bug

--- GeneralHashFunctions.py
+++ GeneralHashFunctions.py
@@ -14,15 +14,17 @@
 #*                                                                        *
 #**************************************************************************
 #
-
+import time
 
 def RSHash(key):
     a    = 378551
     b    =  63689
     hash =      0
     for i in range(len(key)):
-      hash = hash * a + ord(key[i])
-      a = a * b
+        hash = hash * a + ord(key[i])
+        a = a * b
+        a &= ((1 << 32) - 1)
+        hash &= ((1 << 32) - 1)
     return hash
 
 
@@ -114,3 +116,14 @@ def APHash(key):
       else:
         hash ^= (~((hash << 11) + ord(key[i]) ^ (hash >> 5)))
     return hash
+
+if __name__ == "__main__":
+    url = u'http://iip.nju.edu.cn/index.phpntoquery=amp%253Breturnto%3D%25E7%2589%25B9%25E6%25AE%258A%253A%25E7%2594%25A8%25E6%2588%25B7%25E7%2599%25BB%25E5%25BD%2595%26amp%253Breturntoquery%3Damp%25253Breturnto%253D%2525E7%252589%2525B9%2525E6%2525AE%25258A%25253A%2525E7%252594%2525A8%2525E6%252588%2525B7%2525E7%252599%2525BB%2525E5%2525BD%252595%2526amp%25253Breturntoquery%253Damp%2525253Bamp%2525253Breturnto%25253D%252525E7%25252589%252525B9%252525E6%252525AE%2525258A%2525253A%252525E7%25252594%252525A8%252525E6%25252588%252525B7%252525E7%25252599%252525BB%252525E5%252525BD%25252595%252526amp%2525253Bamp%2525253Breturntoquery%25253Damp%252525253Breturnto%2525253D%25252525E7%2525252589%25252525B9%25252525E6%25252525AE%252525258A%252525253A%25252525E7%2525252594%25252525A8%25252525E6%2525252588%25252525B7%25252525E8%25252525B4%25252525A1%25252525E7%252525258C%25252525AE%252525252FLhy'
+
+    hash_list = [RSHash, JSHash, PJWHash, ELFHash, BKDRHash,
+                 SDBMHash, DJBHash, DEKHash, BPHash, FNVHash, APHash]
+    for hash_func in hash_list:
+        start = time.time()
+        value = hash_func(url)
+        end = time.time() - start
+        print(hash_func.func_name, end, len(str(value)))
